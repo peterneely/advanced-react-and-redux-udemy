c0ba9cbb91192b4b8f4cdbb5fb18025a757a93ef
Peter Neely <pgneely@gmail.com>
2016-06-05 20:39:11 -0700

    First passing test

--- redux-simple-starter/test/components/app_test.js
+++ redux-simple-starter/test/components/app_test.js
@@ -1,14 +1,10 @@
-import { renderComponent , expect } from '../test_helper';
+import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
 
-describe('App' , () => {
-  let component;
+describe('App', () => {
 
-  beforeEach(() => {
-    component = renderComponent(App);
-  });
-
-  it('renders something', () => {
-    expect(component).to.exist;
+  it('shows the correct text', () => {
+		const component = renderComponent(App);
+		expect(component).to.contain('React simple starter');
   });
 });
