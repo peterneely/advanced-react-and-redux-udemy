0a9125561398f65685ffa4ba2b1fe135e4a6d984
Peter Neely <pgneely@gmail.com>
2016-06-15 20:59:52 -0700

    wip tests

--- redux-simple-starter/test/test_helper.js
+++ redux-simple-starter/test/test_helper.js
@@ -1,13 +1,24 @@
 import jsdom from 'jsdom';
 import _$ from 'jquery';
-import TestUtil from 'react-addons-test-utils';
+import TestUtils from 'react-addons-test-utils';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import { expect } from 'chai';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from '../src/reducers';
 
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
 const $ = _$(global.window);
 
 function renderComponent(ComponentClass) {
-  const componentInstance = TestUtils.renderIntoDocument(<ComponentClass />);
+  const componentInstance = TestUtils.renderIntoDocument(
+    <Provider store={createStore(reducers)}>
+      <ComponentClass />
+    </Provider>
+  );
   return $(ReactDOM.findDOMNode(componentInstance));
 }
+
+export { renderComponent, expect };
