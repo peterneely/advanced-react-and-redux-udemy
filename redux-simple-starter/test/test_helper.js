c70c135ce46e7fcb2636c525acb66221a721590e
Peter Neely <pgneely@gmail.com>
2016-06-16 21:40:21 -0700

    Finished re-doing test_helper

--- redux-simple-starter/test/test_helper.js
+++ redux-simple-starter/test/test_helper.js
@@ -3,7 +3,8 @@ import _$ from 'jquery';
 import TestUtils from 'react-addons-test-utils';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
+import chaiJquery from 'chai-jquery';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from '../src/reducers';
@@ -12,13 +13,22 @@ global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
 const $ = _$(global.window);
 
-function renderComponent(ComponentClass) {
+function renderComponent(ComponentClass, props, state) {
   const componentInstance = TestUtils.renderIntoDocument(
-    <Provider store={createStore(reducers)}>
-      <ComponentClass />
+    <Provider store={createStore(reducers, state)}>
+      <ComponentClass {...props} />
     </Provider>
   );
   return $(ReactDOM.findDOMNode(componentInstance));
 }
+
+$.fn.simulate = function(eventName, value) {
+  if (value) {
+    this.val(value);
+  }
+  TestUtils.Simulate[eventName](this[0]);
+}
+
+chaiJquery(chai, chai.util, $);
 
 export { renderComponent, expect };
