5a63829d469a9c34d98f947b1787d4faa4611a16
Peter Neely <pgneely@gmail.com>
2016-06-18 08:54:17 -0700

    Must be authenticated to see resources page

--- redux-simple-starter/src/index.js
+++ redux-simple-starter/src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, Route, browserHistory } from 'react-router';
 
+import requireAuth from './components/require_authentication';
 import App from './components/app';
 import Resources from './components/resources';
 import reducers from './reducers';
@@ -14,7 +15,7 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router history={browserHistory}>
 			<Route path="/" component={App}>
-				<Route path="resources" component={Resources}></Route>
+				<Route path="resources" component={requireAuth(Resources)}></Route>
 			</Route>
     </Router>
   </Provider>,
