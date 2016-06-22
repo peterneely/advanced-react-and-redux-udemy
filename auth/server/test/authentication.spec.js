e8f79ca063d14193102dcb32f999a4692b422a91
Peter Neely <pgneely@gmail.com>
2016-06-22 09:30:56 -0700

    Passing dummy tests

--- auth/server/test/authentication.spec.js
+++ auth/server/test/authentication.spec.js
@@ -5,35 +5,35 @@ var server;
 
 describe('authentication', () => {
 
-    describe('signup', () => {
+  describe('signup', () => {
 
-        beforeEach(() => {
-            server = require('../index');
-        });
-
-        afterEach((done) => {
-            server.close(done);
-        });
-
-        it('should see if a user with the given email exists', (done) => {
-            request(server).get('/signup').expect(200, done);
-        });
-
-        it('404 everything else', (done) => {
-            console.log('test 404')
-            request(server).get('/foo/bar').expect(404, done);
-        });
-
-        // it('should return an error if a user with the given email does exist', () => {
-
-        // });
-
-        // it('should create and save the record if a user with the given email does not exist', () => {
-
-        // });
-
-        // it('should respond to the request indicating that the user was created', () => {
-
-        // });
+    beforeEach(() => {
+      server = require('../index');
     });
+
+    afterEach(() => {
+      server.close();
+    });
+
+    it('should see if a user with the given email exists', (done) => {
+      request(server).get('/signup').expect(200, done);
+    });
+
+    it('404 everything else', (done) => {
+      console.log('test 404')
+      request(server).get('/foo/bar').expect(404, done);
+    });
+
+    // it('should return an error if a user with the given email does exist', () => {
+
+    // });
+
+    // it('should create and save the record if a user with the given email does not exist', () => {
+
+    // });
+
+    // it('should respond to the request indicating that the user was created', () => {
+
+    // });
+  });
 });
