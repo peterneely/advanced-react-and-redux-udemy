653abe7e684a2d267e23eadaf96248da38e86bdf
Peter Neely <pgneely@gmail.com>
2016-07-05 15:52:35 -0700

    signup validation

--- client/src/components/auth/signup.js
+++ client/src/components/auth/signup.js
@@ -8,29 +8,40 @@ class Signup extends Component {
     const { handleSubmit, fields: { email, password, passwordConfirm } } = this.props;
     return (
       <form>
-      	<fieldset className="form-group">
-      		<label>Email:</label>
-      		<input className="form-control" {...email} />
-      	</fieldset>
-      	<fieldset className="form-group">
-      		<label>Password:</label>
-      		<input type="password" className="form-control" {...password} />
-      	</fieldset>
-      	<fieldset className="form-group">
-      		<label>Confirm Password:</label>
-      		<input type="password" className="form-control" {...passwordConfirm} />
-      	</fieldset>
+      	{this._renderField({...email, label: 'Email'})}
+      	{this._renderField({...password, label: 'Password'})}
+      	{this._renderField({...passwordConfirm, label: 'Confirm Password'})}
       	<button className="btn btn-primary">Sign up!</button>
       </form>
     );
   }
+
+  _renderField(field) {
+    return (
+      <fieldset className="form-group">
+    		<label>{`${field.label}:`}</label>
+    		<input className="form-control" {...field} />
+    		{field.touched && field.error && <div className="error">{field.error}</div>}
+    	</fieldset>
+    );
+  }
 }
 
-export default reduxForm(createOptions())(Signup);
+export default reduxForm(createOptions(), null, actions)(Signup);
 
 function createOptions() {
   return {
     form: 'signup',
-    fields: ['email', 'password', 'passwordConfirm']
+    fields: ['email', 'password', 'passwordConfirm'],
+    validate
   };
+
+  function validate(formProps) {
+    const errors = {};
+    if (!formProps.email) errors.email = 'Please enter an email';
+    if (!formProps.password) errors.password = 'Please enter a password';
+    if (!formProps.passwordConfirm) errors.passwordConfirm = 'Please confirm your password';
+    if (formProps.password != formProps.passwordConfirm) errors.password = 'Passwords must match';
+    return errors;
+  }
 }
